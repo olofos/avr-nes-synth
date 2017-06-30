cfe125cc61d28305b793f92eb91d70d245e3b387
Olof Ohlsson Sax <olof.ohlsson.sax@gmail.com>
2017-06-30 22:29:45 +0200

    Fixed a glitch in the output of the pulse and noise channels
    When the volume was changed while the channel was outputting a high,
    the wrong volume was ouput until the next time the output went low.
    Previously the volume was simply ORed into the output register, but
    now the register is first cleared.

--- channel/interrupt.asm
+++ channel/interrupt.asm
@@ -27,30 +27,27 @@ not_square:
         rjmp    triangle
         
 noise:  
-        mov     temp1, shift_register_lo ; 1
-        bst     shift_register_lo, 6 ; 1
+        mov     temp1, shift_register_lo             ; 1
+        bst     shift_register_lo, 6                 ; 1
         sbis    _SFR_IO_ADDR(GPIOR0), SHIFT_MODE_BIT ; 2/1
-        bst     shift_register_lo, 1 ; 1
+        bst     shift_register_lo, 1                 ; 1
         ;; the first feedbackbit is now in T
 
         ;; We don't need to zero temp2 first since we only care about bit 0
-        bld     temp2, 0                ; 1 
-        eor     temp1, temp2            ; 1
+        bld     temp2, 0                             ; 1 
+        eor     temp1, temp2                         ; 1
 
-        bst     temp1, 0                ; 1
-        bld     shift_register_hi, 7    ; 1
+        bst     temp1, 0                             ; 1
+        bld     shift_register_hi, 7                 ; 1
 
-        lsr     shift_register_hi       ; 1
-        ror     shift_register_lo       ; 1
+        lsr     shift_register_hi                    ; 1
+        ror     shift_register_lo                    ; 1
 
-        in      temp1, _SFR_IO_ADDR(PINS_DAC_PORT) ; 1
-        or      temp1, channel_volume   ; 1
-        sbrc    shift_register_lo, 0    ; 2/1
-
-output_low:
-        andi    temp1, 0xF0             ; 1
-
-        ;; 14 cycles
+        in      temp1, _SFR_IO_ADDR(PINS_DAC_PORT)   ; 1
+        andi    temp1, 0xF0                          ; 1
+        sbrs    shift_register_lo, 0                 ; 2/1
+        or      temp1, channel_volume                ; 1
+                                                     ; 14 cycles
         
 output: 
         out      _SFR_IO_ADDR(PINS_DAC_PORT), temp1
@@ -91,7 +88,8 @@ square:
         ;; if (channel_step < channel_duty_cycle) output = vol; else output = 0;
         cp      temp1, channel_duty_cycle
         in      temp1, _SFR_IO_ADDR(PINS_DAC_PORT)
-        brcc    output_low
+        andi    temp1, 0xF0
+        brcc    output
         or      temp1, channel_volume
         rjmp    output
         
