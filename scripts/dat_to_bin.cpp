e23476b174b766d83326ab4d2d1418d36882aac9
Olof Ohlsson Sax <olof.ohlsson.sax@gmail.com>
2017-07-19 14:20:03 +0200

    * Use dat_file class in dat_to_bin

--- scripts/dat_to_bin.cpp
+++ scripts/dat_to_bin.cpp
@@ -1,166 +1,52 @@
-#include <cstdio>
-#include <cstdlib>
-
 #include <vector>
+#include <iostream>
 
-#define BEGIN_FRAME    0xf0
-#define END_FRAME      0xf1
-#define END_SONG       0xff
-#define LOOP           0xfe
-
-
-struct reg_t
-{
-    unsigned address;
-    unsigned value;
-};
-
-bool operator==(const reg_t& reg1, const reg_t& reg2)
-{
-    return (reg1.address == reg2.address) && (reg1.value == reg2.value);
-}
-
-bool operator!=(const reg_t& reg1, const reg_t& reg2)
-{
-    return !(reg1 == reg2);
-}
-
-struct frame_t
-{
-    int num;
-    std::vector<reg_t> regs;
-};
-
-bool operator==(const frame_t& frame1, const frame_t& frame2)
-{
-    return frame1.regs == frame2.regs;    
-}
-
-bool operator!=(const frame_t& frame1, const frame_t& frame2)
-{
-    return frame1.regs != frame2.regs;    
-}
+#include "dat_file.h"
 
 int main(int argc, char *argv[])
 {
-    const char *filename = "out.dat";
-    const char *out_filename = "out.bin";
+    std::string filename_in = "out.dat";
+    std::string filename_out = "out.bin";
 
     if(argc > 1)
     {
-        filename = argv[1];
+        filename_in = argv[1];
     }
 
     if(argc > 2)
     {
-        out_filename = argv[2];
-    }
-    
-    FILE *file = std::fopen(filename, "r");
-
-    if(!file)
-    {
-        char str[256];
-        std::snprintf(str, 256, "Error opening '%s'", filename);
-        std::perror(str);
-        std::exit(1);
+        filename_out = argv[2];
     }
 
-    bool song_done = false;
-    int frame_count = 0;
+    DatFile dat_file;
 
-    std::vector<frame_t> frames;
+    dat_file.load_ascii(filename_in);
 
-    while(!song_done)
+    Frame &last_frame = dat_file.frames.back();
+
+    if(!last_frame.regs.empty() && last_frame.regs[0].address == LOOP_FRAME)
     {
-        bool frame_done = false;
+        int loop_frame_dest = (last_frame.regs[1].address << 8) | last_frame.regs[1].value;
+        std::cout << "Loop to frame " << loop_frame_dest << "\n";
 
-        frame_t frame;
-        frame.num = frame_count;
+        int loop_byte_dest = 0;
 
-        while(!frame_done) {
-            unsigned address;
-            unsigned value;
-
-            if(!std::fscanf(file, "0x%02x,\t0x%02x,\n", &address, &value))
-            {
-                std::perror("Error reading file");
-                std::exit(1);
-            }
-
-            if(address == END_SONG)
-            {
-                frame_done = true;
-                song_done  = true;
-            } else if(address == END_FRAME) {
-                frame_done = true;
-            } else {
-                reg_t reg;
-                reg.address = address;
-                reg.value = value;
-                
-                frame.regs.push_back(reg);
-            }
-        }
-
-        frames.push_back(frame);
-        frame_count++;
-    }
-
-    std::fclose(file);
-
-    std::printf("Read %d frames\n", frame_count);
-
-    FILE *of = std::fopen(out_filename, "wb");
-
-    int loop_frame = -1;
-
-    for(size_t i = 0; i < frames.size(); i++)
-    {
-        auto frame = frames[i];
-
-        if(frame.regs.size() > 0 && frame.regs[0].address == LOOP)
+        for(int i = 0; i < loop_frame_dest; i++)
         {
-            loop_frame = (frame.regs[1].address << 8) | frame.regs[1].value;
-        } else {
-            for(auto reg: frame.regs)
-            {
-                fwrite(&reg.address, 1, 1, of);
-                fwrite(&reg.value, 1, 1, of);
-            }
-
-            unsigned char b = END_FRAME;
-
-            fwrite(&b, 1, 1, of);
-            fwrite(&b, 1, 1, of);
-        }
-    }
-
-    if(loop_frame >= 0)
-    {
-        unsigned byte_count = 0;
-
-        for(int i = 0; i < loop_frame; i++)
-        {
-            byte_count += 2*(frames[i].regs.size()+1);
+            loop_byte_dest += dat_file.frames[i].regs.size() * 2;
         }
 
-        std::printf("Loop at frame %d (0x%08X bytes)\n", loop_frame, byte_count);
+        std::cout << "Loop to byte " << loop_byte_dest << "\n";
 
-        unsigned char b = LOOP;
-        fwrite(&b, 1, 1, of);
-        fwrite(&b, 1, 1, of);
+        dat_file.frames.pop_back();
 
-        b = (byte_count >> 8) & 0xFF;
-        fwrite(&b, 1, 1, of);
-        b = byte_count & 0xFF;
-        fwrite(&b, 1, 1, of);
+        Frame loop_frame;
+
+        loop_frame.regs.push_back(Reg(LOOP_BYTE, LOOP_BYTE));
+        loop_frame.regs.push_back(Reg((loop_byte_dest >> 8)  & 0xFF , loop_byte_dest & 0xFF));
+
+        dat_file.frames.push_back(loop_frame);
     }
 
-    unsigned char b = END_SONG;
-
-    fwrite(&b, 1, 1, of);
-    fwrite(&b, 1, 1, of);
-    
-    fclose(of);
+    dat_file.save_binary(filename_out);
 }
