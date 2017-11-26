ee4bd6b9e431201e62e879f91eb1aa866bab82bb
Olof Ohlsson Sax <olof.ohlsson.sax@gmail.com>
2017-11-26 15:39:13 +0100

    Added 48x48 pixel logo

---
+++ lib/logo-paw-48x48.h
@@ -0,0 +1,6 @@
+#ifndef LOGO_PAW_48_48_H_
+#define LOGO_PAW_48_48_H_
+
+extern const uint8_t paw_48x48[] PROGMEM;
+
+#endif
--- lib/logo-paw-64x64.h
+++ lib/logo-paw-64x64.h
@@ -1,5 +1,5 @@
-#ifndef LOGO_PAW_H_
-#define LOGO_PAW_H_
+#ifndef LOGO_PAW_64_64_H_
+#define LOGO_PAW_64_64_H_
 
 extern const uint8_t paw_64x64[] PROGMEM;
 
