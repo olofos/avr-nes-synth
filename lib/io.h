8325e88cf70a1c939b1078ddea5ead7149984b9c
Olof Ohlsson Sax <olof.ohlsson.sax@gmail.com>
2017-12-07 00:20:20 +0100

    Don't read the port in set_outputs, set_inputs, enable_pullups, disable_pullups if writing to the whole port

--- lib/io.h
+++ lib/io.h
@@ -17,10 +17,10 @@
 #define is_high(pin)        (pin ## _PIN & _BV(pin))
 #define is_low(pin)         (!(pin ## _PIN & _BV(pin)))
 
-#define set_outputs(pins) {pins ## _DDR |= (pins ## _MASK);}
-#define set_inputs(pins)  {pins ## _DDR &= ~(pins ## _MASK);}
-#define enable_pullups(pins)  {pins ## _PORT |= (pins ## _MASK);}
-#define disable_pullups(pins) {pins ## _PORT &= ~(pins ## _MASK);}
+#define set_outputs(pins) do {if(pins ## _MASK == 0xFF) pins ## _DDR = 0xFF; else pins ## _DDR |= (pins ## _MASK);} while(0)
+#define set_inputs(pins)  do {if(pins ## _MASK == 0xFF) pins ## _DDR = 0x00; else pins ## _DDR &= ~(pins ## _MASK);} while(0)
+#define enable_pullups(pins)  do {if(pins ## _MASK == 0xFF) pins ## _PORT = 0xFF; else pins ## _PORT |= (pins ## _MASK);} while(0)
+#define disable_pullups(pins) do {if(pins ## _MASK == 0xFF) pins ## _PORT = 0x00; else pins ## _PORT &= ~(pins ## _MASK);} while(0)
 
 #define are_high(pins) (pins ## _PIN & pins ## _MASK)
 #define are_low(pins) ((~(pins ## _PIN)) & pins ## _MASK)
